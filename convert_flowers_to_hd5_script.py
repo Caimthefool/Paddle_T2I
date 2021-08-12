d5e584a24aa12f0c4adf1c9c7c02efe311c17dcb
Caimthefool <88370100+Caimthefool@users.noreply.github.com>
2021-08-12 13:53:04 +0800

    Update convert_flowers_to_hd5_script.py

--- convert_flowers_to_hd5_script.py
+++ convert_flowers_to_hd5_script.py
@@ -11,16 +11,17 @@ import pdb
 
 with open('config.yaml', 'r') as f:
 	config = yaml.load(f)
-
+# get the dataset path
 images_path = config['flowers_images_path']
 embedding_path = config['flowers_embedding_path']
 text_path = config['flowers_text_path']
 datasetDir = config['flowers_dataset_path']
-
+# get the name of classes
 val_classes = open(config['flowers_val_split_path']).read().splitlines()
 train_classes = open(config['flowers_train_split_path']).read().splitlines()
 test_classes = open(config['flowers_test_split_path']).read().splitlines()
-
+# create the save group
+# save the different splits of dataset into different group
 f = h5py.File(datasetDir, 'w')
 train = f.create_group('train')
 valid = f.create_group('valid')
@@ -34,7 +35,7 @@ for _class in sorted(os.listdir(embedding_path)):
 		split = valid
 	elif _class in test_classes:
 		split = test
-
+	# get the images and embeddings
 	data_path = os.path.join(embedding_path, _class)
 	txt_path = os.path.join(text_path, _class)
 	for example, txt_file in zip(sorted(glob(data_path + "/*.t7")), sorted(glob(txt_path + "/*.txt"))):
@@ -56,7 +57,7 @@ for _class in sorted(os.listdir(embedding_path)):
 		txt = np.array(txt)
 		txt = txt[txt_choice]
 		dt = h5py.special_dtype(vlen=str)
-
+		# save the images and embeddings
 		for c, e in enumerate(embeddings):
 			ex = split.create_group(example_name + '_' + str(c))
 			ex.create_dataset('name', data=example_name)
